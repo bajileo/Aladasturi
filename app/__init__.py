bc779a8506947764b51aeba955dac803a7e65349
bajileo <144520260+bajileo@users.noreply.github.com>
2024-06-13 19:06:11 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from extensions import app
-from routes import samsung,apple,lenovo,asus, home, about, dicte, review, sponsors, layout, add_review, register, login, discount, logout, used_SAMSUNG_discount, used_APPLE_discount, used_LENOVO_discount, used_ASUS_discount
+from routes import *
